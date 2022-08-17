49b90d80d6b28ce2e0dad871e5b76f08cc6e1643
Lawson Allick <lallick@gmail.com>
2022-08-17 14:44:15 -0400

    Add html interaction to the game

--- script.js
+++ script.js
@@ -16,9 +16,35 @@ const game = (() => {
 
     const play = () => {
         let turn = playerOne
-        let button = document.querySelector('button')
-        let xCoordinate = document.getElementById('x-coordinate')
-        let yCoordinate = document.getElementById('y-coordinate')
+
+
+        const boxes = document.querySelectorAll('.box')
+        boxes.forEach(box => box.addEventListener('click', () => {
+            let x = parseInt(box.id[0])
+            let y = parseInt(box.id[1])
+            let valid = checkValid(x, y)
+            if (valid) {
+                placeMarker(turn, x, y)
+                box.textContent = turn.mark
+                if (checkWinner(turn)) {
+                    console.log(turn)
+                    console.log('Wins')
+                } else if (tieGame()) {
+                    console.log('Tie')
+                } else {
+                    if (turn === playerOne) {
+                        turn = playerTwo
+                    } else {
+                        turn = playerOne
+                    }
+                }
+            } else {
+                console.log('Not a valid spot')
+                return
+            }
+        }
+        ))
+
 
         //function to check if a valid position has been chosen
         const checkValid = (posX, posY) => {
@@ -48,6 +74,7 @@ const game = (() => {
             const allEqual = (arr, marker) => arr.every(v => v === marker)
             for (let i = 0; i < 3; i++) {
                 if (allEqual(gameBoard.board[i], potentialWinner.mark)) {
+                    console.log('horizontal win')
                     return true
                 }
             }
@@ -58,23 +85,25 @@ const game = (() => {
                     testArr.push(gameBoard.board[i][p])
                 }
                 if (allEqual(testArr, potentialWinner.mark)) {
+                    console.log('vertical win')
                     return true
                 }
             }
 
+            let testArr1 = []
+            let testArr2 = []
             for (let i = 0; i < 3; i++) {
-                let testArr = []
-                let testArr2 = []
-                testArr.push(gameBoard.board[i][i])
-                testArr2.push(gameBoard.board[i][2 - i])
-                if (i === 2) {
-                    if (allEqual(testArr, potentialWinner.mark)) {
-                        return true
-                    }
-                    if (allEqual(testArr2, potentialWinner.mark)) {
-                        return true
-                    }
-                }
+                testArr1.push(gameBoard.board[i][i])
+                testArr2.push(gameBoard.board[2 - i][i])
+            }
+
+            if (allEqual(testArr1, potentialWinner.mark)) {
+                console.log('diagonal win 1')
+                return true
+            } else if (allEqual(testArr2, potentialWinner.mark)) {
+                console.log(testArr2)
+                console.log('diagonal win 2')
+                return true
             }
             return false
         }
@@ -82,7 +111,7 @@ const game = (() => {
         //function to check for tie game
         const tieGame = () => {
             for (let i = 0; i < 3; i++) {
-                for(let p = 0; p < 3; p++) {
+                for (let p = 0; p < 3; p++) {
                     if (gameBoard.board[i][p] === '_') {
                         return false
                     }
@@ -91,34 +120,9 @@ const game = (() => {
             return true
         }
 
-        button.addEventListener('click', (e) => {
-            e.preventDefault()
-            let x = xCoordinate.value
-            let y = yCoordinate.value
-            let valid = checkValid(x, y)
-            if (valid) {
-                placeMarker(turn, x, y)
-                if(checkWinner(turn)){
-                    console.log(turn)
-                    console.log('Wins')
-                } else if (tieGame()){
-                    console.log('Tie')
-                } else {
-                    if (turn === playerOne) {
-                        turn = playerTwo
-                    } else {
-                        turn = playerOne
-                    }
-                }
-            } else{
-                console.log('Not a valid spot')
-                return
-            }
-        })
-        
 
     }
-    return {play}
+    return { play }
 
 })();
 
